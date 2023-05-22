207fb3c3d2ac00899bb201641e065f28453b0d46
caixypromise <caixypromised@gmail.com>
2023-05-22 22:29:57 +0800

    添加科大讯飞文字转写接口,完成项目编写

--- test.py
+++ test.py
@@ -14,13 +14,14 @@ headers = {
 json_data = {
     'name': 'example_filename'
 }
-
+# 请求文字识别，并在服务器内生成音频文件
 response = requests.post(url, files=files)
-
+# 如果生成成功
 if response.status_code == 200:
     ret_json = response.json()
     print(f'response return: {response.json}')
     json_data['name'] = ret_json['message']
+    # 请求下载音频文件
     response = requests.post(download_url, headers = headers, json = json_data)
     if (response.status_code == 200):
         with open(f"{ret_json['message']}.wav", 'wb') as f:
